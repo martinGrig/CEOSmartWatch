d1f8ee48f0ad127d4d52e43459c621a62708695c
Martin <400678@student.fontys.nl>
2020-06-23 16:16:46 +0300

    git ignore

---
+++ app/src/main/java/com/example/smartwatchapp/FinanceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smartwatchapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finance)
+    }
+}
